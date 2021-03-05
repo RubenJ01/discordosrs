5ddac1c2aa9ff2cafbdfbb5badcf99cac47bacc1
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2021-03-05 20:42:17 +0100

    Has character check used, but incorrectly

--- cogs/character_creation.py
+++ cogs/character_creation.py
@@ -3,9 +3,9 @@ from discord import message
 from discord.ext.commands import Cog, command
 from discord.ext import commands
 import discord
-
 from backend.conn import cur, conn, db
 from backend.helpers import sql_query, sql_edit
+from backend.checks import has_character
 
 
 class CharacterCreation(Cog, name="Character Creation"):
@@ -26,19 +26,27 @@ class CharacterCreation(Cog, name="Character Creation"):
     @character.command(name="create")
     async def create_character(self, ctx, character_name):
         discord_id = ctx.author.id
-        print(character_name, discord_id)
-        # create character with the name from the command and the discord id of the author
-        await sql_edit("INSERT INTO characters (name, discord_id) VALUES(?, ?)", (character_name, discord_id,))
+        # check if the user already has a character
+        print(has_character)
+        if has_character():
+            message = discord.Embed(
+                title="Character Creating failed",
+                description=str(
+                    "Unable to create character, you already have a character")
+            )
+        else:
+            # create character with the name from the command and the discord id of the author
+            await sql_edit("INSERT INTO characters (name, discord_id) VALUES(?, ?)", (character_name, discord_id,))
 
-        message = discord.Embed(
-            title="Character Created",
-            description=str(
-                "You have created a character called " + character_name)
-        )
+            message = discord.Embed(
+                title="Character Created",
+                description=str(
+                    "You have created a character called " + character_name)
+            )
         return await ctx.send(embed=message)
     # command for deleting a character
 
-    @character.command(name="delete")
+    @ character.command(name="delete")
     async def delete_character(self, ctx):
         discord_id = ctx.author.id
         # ask to confirm that the auther wants to delete the character
@@ -52,6 +60,7 @@ class CharacterCreation(Cog, name="Character Creation"):
             msg = await self.bot.wait_for('message', timeout=15.0, check=check)
             if(msg.content in 'Yy'):
                 # if the message from the author contains y or Y, delete the character bound to that discord_id
+
                 await sql_edit("DELETE FROM Characters WHERE discord_id = ?", (discord_id,))
         except asyncio.TimeoutError:
             return await ctx.send('No response. Character reset stopped.')
