0b1c21ccf0d4af92fa5b4a8a10da287c1a381d74
Ruben <rubeneekhof@gmail.com>
2021-03-11 16:00:46 +0100

    Fixed the wintertodt command

--- cogs/minigames/wintertodt.py
+++ cogs/minigames/wintertodt.py
@@ -1,7 +1,6 @@
 import random
 import asyncio
 import time
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from backend.helpers import gained_exp, check_time
 from backend.checks import has_character
@@ -13,105 +12,105 @@ from discord.ext.commands import Cog, command
 class WinterTodt(Cog, name="Wintertodt"):
     def __init__(self, bot):
         self.bot = bot
-        self.wait_time = None
-        self.wintertodt_game_time = None
-        self.points_earned = None
-        self.firemaking_level = None
-        self.woodcutting_level = None
-        self.bruma_roots = None
-        self.burning_bruma_roots = None
-        self.chopping_bruma_roots = None
-        self.firemaking_xp_earned = None
-        self.woodcutting_xp_earned = None
-        self.activity_embed = None
-        self.time_started = None
-        self.time_end = None
 
     async def initialize_game(self):
-        self.wintertodt_game_time = random.choice([300, 310, 320])
-        self.points_earned = 0
-        self.firemaking_level = 50
-        self.woodcutting_level = 50
-        self.bruma_roots = 0
-        self.burning_bruma_roots = False
-        self.chopping_bruma_roots = True
-        self.firemaking_xp_earned = 0
-        self.woodcutting_xp_earned = 0
-
-    async def event(self, ctx):
-        danger = random.randint(1, 101)
-        if danger <= 10:
-            embed = discord.Embed(
-                description="Snow falls upon you interrupting any ongoing activity.")
-            await self.activity_embed.edit(embed=embed)
-        elif self.chopping_bruma_roots:
-            xp_earned = int((self.woodcutting_level * 0.3) * 2)
-            self.bruma_roots += 2
-            embed = discord.Embed(description=f"You chop 2 more bruma roots "
-                                              f"{self.bot.get_emoji(815955317619163156)} "
-                                              f"and add them to your inventory "
-                                              f"({self.bruma_roots} total)"
-                                              f" earning you {xp_earned} woodcutting experience "
-                                              f"{self.bot.get_emoji(815955047011582053)}. ")
-            await self.activity_embed.edit(embed=embed)
-            self.woodcutting_xp_earned += xp_earned
-            if self.bruma_roots == 20:
-                self.burning_bruma_roots = True
-                self.chopping_bruma_roots = False
-        elif self.burning_bruma_roots:
-            xp_earned = (self.firemaking_level * 3) * 2
-            self.points_earned += 20
-            embed = discord.Embed(description=f"You add 2 bruma roots {self.bot.get_emoji(815955317619163156)}"
-                                              f" to the fire earning you "
-                                              f"20 points ({self.points_earned} total) and "
-                                              f"{xp_earned} firemaking experience "
-                                              f"{self.bot.get_emoji(815955047417380874)}.")
-            await self.activity_embed.edit(embed=embed)
-            self.bruma_roots -= 2
-            self.firemaking_xp_earned += xp_earned
-            if self.bruma_roots == 0:
-                self.burning_bruma_roots = False
-                self.chopping_bruma_roots = True
+        wintertodt_game_time = random.choice([300, 310, 320])
+        points_earned = 0
+        firemaking_level = 50
+        woodcutting_level = 50
+        bruma_roots = 0
+        burning_bruma_roots = False
+        chopping_bruma_roots = True
+        firemaking_xp_earned = 0
+        woodcutting_xp_earned = 0
+        return [wintertodt_game_time, points_earned, firemaking_level, woodcutting_level, bruma_roots,
+                burning_bruma_roots, chopping_bruma_roots, firemaking_xp_earned, woodcutting_xp_earned]
 
     async def start_game(self, ctx):
-        await self.initialize_game()
-        sched = AsyncIOScheduler()
-        xp_earned = 6 * self.firemaking_level
+        values = await self.initialize_game()
+        # unpacking all the values
+        wintertodt_game_time = values[0]
+        points_earned = values[1]
+        firemaking_level = values[2]
+        woodcutting_level = values[3]
+        bruma_roots = values[4]
+        burning_bruma_roots = values[5]
+        chopping_bruma_roots = values[6]
+        firemaking_xp_earned = values[7]
+        woodcutting_xp_earned = values[8]
+        xp_earned = 6 * firemaking_level
         activity_embed = discord.Embed(description=f"The wintertodt begins. "
                                                    f"You light the brazier earning you an additional 25 points "
                                                    f"and {xp_earned} firemaking experience "
                                                    f"{self.bot.get_emoji(815955047417380874)}.")
         activity_embed.set_image(url="https://oldschool.runescape.wiki/images/thumb/7/78/Burning_brazier_"
                                      "%28Wintertodt%29.png/687px-Burning_brazier_%28Wintertodt%29.png?7b131")
-        self.activity_embed = await ctx.send(embed=activity_embed)
-        self.points_earned += 25
-        self.firemaking_xp_earned += xp_earned
-        sched.add_job(self.event, 'interval', args=[ctx], seconds=5)
-        sched.start()
-        await asyncio.sleep(self.wintertodt_game_time + 1)
-        sched.shutdown()
-        xp_earned = 100 * self.firemaking_level
-        self.firemaking_xp_earned += xp_earned
-        await gained_exp(ctx, "firemaking", self.firemaking_xp_earned)
-        await gained_exp(ctx, "woodcutting", self.woodcutting_xp_earned)
+        activity_embed = await ctx.send(embed=activity_embed)
+        points_earned += 25
+        firemaking_xp_earned += xp_earned
+        game_started = True
+        time_end = time.time() + (wintertodt_game_time + 1)
+        await asyncio.sleep(5)
+        while game_started:
+            if time.time() > time_end:
+                break
+            else:
+                danger = random.randint(1, 101)
+                if danger <= 10:
+                    embed = discord.Embed(
+                        description="Snow falls upon you interrupting any ongoing activity.")
+                    await activity_embed.edit(embed=embed)
+                elif chopping_bruma_roots:
+                    xp_earned = int((woodcutting_level * 0.3) * 2)
+                    bruma_roots += 2
+                    embed = discord.Embed(description=f"You chop 2 more bruma roots "
+                                                      f"{self.bot.get_emoji(815955317619163156)} "
+                                                      f"and add them to your inventory "
+                                                      f"({bruma_roots} total)"
+                                                      f" earning you {xp_earned} woodcutting experience "
+                                                      f"{self.bot.get_emoji(815955047011582053)}. ")
+                    await activity_embed.edit(embed=embed)
+                    woodcutting_xp_earned += xp_earned
+                    if bruma_roots == 20:
+                        burning_bruma_roots = True
+                        chopping_bruma_roots = False
+                elif burning_bruma_roots:
+                    xp_earned = (firemaking_level * 3) * 2
+                    points_earned += 20
+                    embed = discord.Embed(description=f"You add 2 bruma roots {self.bot.get_emoji(815955317619163156)}"
+                                                      f" to the fire earning you "
+                                                      f"20 points ({points_earned} total) and "
+                                                      f"{xp_earned} firemaking experience "
+                                                      f"{self.bot.get_emoji(815955047417380874)}.")
+                    await activity_embed.edit(embed=embed)
+                    bruma_roots -= 2
+                    firemaking_xp_earned += xp_earned
+                    if bruma_roots == 0:
+                        burning_bruma_roots = False
+                        chopping_bruma_roots = True
+                await asyncio.sleep(5)
+        xp_earned = 100 * firemaking_level
+        firemaking_xp_earned += xp_earned
+        await gained_exp(ctx, "firemaking", firemaking_xp_earned)
+        await gained_exp(ctx, "woodcutting", woodcutting_xp_earned)
         desc = ""
-        if self.points_earned > 500:
+        if points_earned > 500:
             desc += f"You have reached at least 500 points earning you an additional " \
                     f"{xp_earned} firemaking experience. \n\n"
-        desc += f"You earned a total of {self.firemaking_xp_earned} firemaking experience and " \
-                f"{self.woodcutting_xp_earned} woodcutting experience with a total of {self.points_earned} points."
+        desc += f"You earned a total of {firemaking_xp_earned} firemaking experience and " \
+                f"{woodcutting_xp_earned} woodcutting experience with a total of {points_earned} points."
         embed = discord.Embed(title="The wintertodt has been subdued.",
                               description=desc)
         embed.set_image(url="https://oldschool.runescape.wiki/images/thumb/1/15/Howling_Snow_Storm.gif/300px"
                             "-Howling_Snow_Storm.gif?ec549")
-        return await self.activity_embed.edit(embed=embed)
+        await activity_embed.edit(embed=embed)
 
     async def game_pause(self, ctx):
-        self.wait_time = 60
+        wait_time = 60
         embed = discord.Embed(
-            description=f"The next wintertodt game is about to start in {self.wait_time} seconds.")
+            description=f"The next wintertodt game is about to start in {wait_time} seconds.")
         await ctx.send(embed=embed)
-        await asyncio.sleep(self.wait_time)
+        await asyncio.sleep(wait_time)
 
     @has_character()
     @command(name="wintertodt")
@@ -119,12 +118,12 @@ class WinterTodt(Cog, name="Wintertodt"):
         values = check_time(requested_time, 1, 8)
         if values[0] is True:
             embed = discord.Embed(description=f"... begins subdueing the Wintertodt for {requested_time} hour(s).")
-            self.time_started = time.time()
-            self.time_end = time.time() + (requested_time * 3600)
+            time_started = time.time()
+            time_end = time.time() + (requested_time * 3600)
             await ctx.send(embed=embed)
-            while self.time_started < self.time_end:
+            while time_started < time_end:
                 await self.start_game(ctx)
-                if time.time() < self.time_end:
+                if time.time() < time_end:
                     await self.game_pause(ctx)
                 else:
                     break
